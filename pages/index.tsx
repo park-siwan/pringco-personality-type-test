9b9cb1d8fdd0cf8a3954d16c34b4cfd4171cb356
park-siwan <siwandevelop@gmail.com>
2022-11-20 17:17:42 +0900

    style: max-w-sm 으로 모바일사이즈 설정, mx-auto로 가운데 정렬

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,63 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Home() {
+  const [isTestStart, setIsTestStart] = useState(false);
+  /** */
+  const handleTestStart = () => {
+    console.log('on');
+    setIsTestStart(true);
+  };
+  /** */
+  const TestDoc = () => {
+    return (
+      <div className='flex'>
+        <button
+          onClick={handleTestStart}
+          className='bg-red-500 text-white w-full p-3'
+        >
+          테스트 시작하기
+        </button>
+        <button
+          onClick={handleTestStart}
+          className='bg-red-500 text-white w-full p-3'
+        >
+          테스트 시작하기
+        </button>
+      </div>
+    );
+  };
   return (
-    <div>
-      <Head>
-        {/* What is the ball you chose? */}
-        <title>8BALL FRIENDS</title>
-        <meta name='8 ball friends' content='성격 유형 검사 시작' />
-        {/* <link rel='icon' href='/favicon.ico' /> */}
-      </Head>
+    <div className='max-w-sm mx-auto'>
+      <div className='flex items-center justify-center'>
+        <Head>
+          {/* What is the ball you chose? */}
+          <title>8BALL FRIENDS</title>
+          <meta name='8 ball friends' content='성격 유형 검사 시작' />
+          {/* <link rel='icon' href='/favicon.ico' /> */}
+        </Head>
 
-      <main>
-        <h1 className={'text-3xl font-bold underline'}>
-          What is the ball you chose?
-        </h1>
-        <div className='bg-slate-400 py-20 px-10 grid gap-10 min-h-screen'>
-          <div className='bg-white p-10 rounded-3xl shadow-xl'>
-            <span className='font-bold text-2xl'>Select Item</span>
-            <div className='flex justify-between my-2'>
-              <span className='text-gray-500'>Gray Chair</span>
-              <span className='font-bold'>$19</span>
-            </div>
-            <div className='flex justify-between'>
-              <span className='text-gray-500'>Gray Chair</span>
-              <span className='font-bold'>$19</span>
-            </div>
-            <div className='flex justify-between mt-2 pt-2 border-t-2 border-dashed font-bold'>
-              <span>Total</span>
-              <span>$38</span>
-            </div>
+        <main>
+          {isTestStart ? (
+            <TestDoc />
+          ) : (
+            <>
+              <h1 className='text-3xl font-bold text-center'>
+                What is the ball you chose?
+              </h1>
+              <button
+                onClick={handleTestStart}
+                className='bg-red-500 text-white w-full p-3'
+              >
+                테스트 시작하기
+              </button>
+            </>
+          )}
+        </main>
 
-            <button
-              className='flex justify-center w-2/4 mt-5 mx-auto bg-blue-500
-              rounded-2xl p-3 text-white
-              hover:bg-blue-400  active:bg-teal-500  disabled:bg-blue-200'
-              type='button'
-            >
-              Checkout
-            </button>
-          </div>
-        </div>
-      </main>
-
-      <footer>
-        {/* <a
+        <footer>
+          {/* <a
           href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
           target='_blank'
           rel='noopener noreferrer'
@@ -54,7 +67,8 @@ export default function Home() {
             <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
           </span>
         </a> */}
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 }
