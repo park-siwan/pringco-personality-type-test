ac44915cc44f1b15e2caa6f1fd2877bec478db25
park-siwan <siwandevelop@gmail.com>
2022-11-20 18:10:36 +0900

    feat: 성격 유형 선택 기능

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,35 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { useState } from 'react';
 
+type personalityType = 'E' | 'I' | 'N' | 'S' | 'F' | 'T' | 'J' | 'P';
+
 export default function Home() {
   const [isTestStart, setIsTestStart] = useState(false);
-  /** */
+  const [typeCounts, setTypeCounts] = useState({
+    E: 0,
+    I: 0,
+    N: 0,
+    S: 0,
+    F: 0,
+    T: 0,
+    J: 0,
+    P: 0,
+  });
+  /**
+   * @todo
+   * - 12가지의 선택지와 24가지의 질문이 있다.
+   * - 질문은 2개중 택 1이다.
+   * - 질문을 선택하면 해당되는 유형을 카운팅한다.
+   * - 12번의 질문이 끝나면 최종 카운팅된 유형 조합에 맞춰 유형을 보여준다.
+   * - 공유 기능이 있어야 한다.
+   */
+  const handleTestCount = (type: personalityType) => {
+    console.log('type', type);
+    setTypeCounts({ ...typeCounts, [type]: typeCounts[type] + 1 });
+  };
+  console.log('typeCounts', typeCounts);
+
   const handleTestStart = () => {
-    console.log('on');
     setIsTestStart(true);
   };
   const btnStyle = 'bg-red-500 text-white w-full p-6 rounded-full mb-3';
@@ -19,10 +43,10 @@ export default function Home() {
           친구와 약속이 취소되었을때 나는
         </h1>
         <div className={'flex flex-col'}>
-          <button onClick={handleTestStart} className={btnStyle}>
+          <button onClick={() => handleTestCount('E')} className={btnStyle}>
             다른 친구에게 연락해서 나오라고 한다
           </button>
-          <button onClick={handleTestStart} className={btnStyle}>
+          <button onClick={() => handleTestCount('I')} className={btnStyle}>
             혼자 집에서 뒹굴뒹굴 거린다
           </button>
         </div>
