177c7ee65d5154cea9ef69a7731f50af586d2794
park-siwan <siwandevelop@gmail.com>
2022-11-20 19:24:09 +0900

    style: \n으로 줄바꿈 하는 대신 문자열보간으로 줄바꿈하고 pre-line스타일 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 const mockupData = [
   {
     title: '친구와 약속이 취소되었을때 나는',
-    firstQuestion: '다른 친구에게 연락해서 나오라고 한다',
+    firstQuestion: `다른 친구에게 연락해서 나오라고 한다`,
     lastQuestion: '다른 친구에게 연락해서 나오라고 한다',
   },
   {
@@ -19,48 +19,65 @@ const mockupData = [
   },
   {
     title: '시험기간, 업무 등 중요한 일이 있을 때 나는',
-    firstQuestion: `"오늘은 거기까지 해야겠다."\n계획대로 차근차근 진행한다.`,
-    lastQuestion: `"아직 시간 많이남았네~"\n 놀다가 벼락치기 한다.`,
+    firstQuestion: `"오늘은 거기까지 해야겠다."
+    계획대로 차근차근 진행한다.`,
+    lastQuestion: `"아직 시간 많이남았네~"
+    놀다가 벼락치기 한다.`,
   },
   {
     title: '사람이 많은 길거리에 넘어졌을 때 나는',
-    firstQuestion: `"아야! 뭐야 이건??"\n 괜히 큰소리 친다`,
+    firstQuestion: `"아야! 뭐야 이건??"
+    괜히 큰소리 친다`,
     lastQuestion: `"(머쓱;;) 꾹 참고 가던길 간다."`,
   },
   {
     title: '내가 좋아하는 것에 대해 설명할 때 나는',
-    firstQuestion: `"그건 말이야,, 이러쿵 저러쿵,,\n 구체적으로 설명한다."`,
-    lastQuestion: `"넘나 좋은 것!"\n 단순하게 설명한다.`,
+    firstQuestion: `"그건 말이야,, 이러쿵 저러쿵,,
+    구체적으로 설명한다."`,
+    lastQuestion: `"넘나 좋은 것!"
+    단순하게 설명한다.`,
   },
   {
     title: '친구와 사이좋게 먹던 디저트가 한조각 남았을 때 나는',
-    firstQuestion: `가위바위보 할래?\n정정당당하게 맞선다`,
-    lastQuestion: `"너 먹어~"\n양보한다`,
+    firstQuestion: `가위바위보 할래?
+    정정당당하게 맞선다`,
+    lastQuestion: `"너 먹어~"
+    양보한다`,
   },
   {
     title: '휴가 계획을 짤 때 나는',
-    firstQuestion: '7시 30분까지는 관람을 마쳐야해\n구체적으로 짠다',
-    lastQuestion: `"이날은 먹방데이~~~\n대충 테마만 생각해둔다."`,
+    firstQuestion: `"7시 30분까지는 관람을 마쳐야해"
+    구체적으로 짠다`,
+    lastQuestion: `"이날은 먹방데이~~~"
+    대충 테마만 생각해둔다."`,
   },
   {
     title: '오랜만에 맛집을 찾았을 때 나는',
-    firstQuestion: `"여러분 이 과자 좀 보세요!!!!!!!!!!"\n 동네방네 소문내며 추천한다.`,
-    lastQuestion: `오~ 제법 하는데~?\n혼자 생각한다.`,
+    firstQuestion: `"여러분 이 과자 좀 보세요!!!!!!!!!!"
+    동네방네 소문내며 추천한다.`,
+    lastQuestion: `오~ 제법 하는데~?
+    혼자 생각한다.`,
   },
   {
     title: '고를 때 나는 ',
-    firstQuestion: '역시 이게 짱이지!\n최애를 선택한다.',
-    lastQuestion: `"오? 새로나온 과자네?"\n신상을 선택한다.`,
+    firstQuestion: `역시 이게 짱이지!"
+    최애를 선택한다.`,
+    lastQuestion: `"오? 새로나온 과자네?"
+    신상을 선택한다.`,
   },
   {
     title: '조별과제를 할 때 나는',
-    firstQuestion: `"무임승차 OUT!"\n제대로 못하는 팀원들을 제명한다`,
-    lastQuestion: `"사정이 있겠지..."\n한 번 봐준다`,
+    firstQuestion: `"무임승차 OUT!"
+    제대로 못하는 팀원들을 제명한다`,
+    lastQuestion: `"사정이 있겠지..."
+    한 번 봐준다`,
   },
   {
     title: '친구가 우리집에 놀러와서 물건을 찾을 때 나는',
-    firstQuestion: `"그거 왼쪽 서랍 세번째 칸에 있어"\n정확히 알고 있다.`,
-    lastQuestion: `"음 책상 쪽 한번 잘 봐봐\n대충은 안다"`,
+    firstQuestion: `"그거 왼쪽 서랍 세번째 칸에 있어"
+    정확히 알고 있다.`,
+    lastQuestion: `"음 책상 쪽 한번 잘 봐봐
+    대충은 안다"`,
   },
 ];
 type personalityType = 'E' | 'I' | 'N' | 'S' | 'F' | 'T' | 'J' | 'P';
@@ -103,15 +120,13 @@ export default function Home() {
   const handleTestStart = () => {
     setIsTestStart(true);
   };
-  const btnStyle = 'bg-red-500 text-white w-full p-6 rounded-xl mb-3 h-32';
-  /** */
+  /**
+   * - pre-line 으로 줄바꿈 표현
+   * @link https://aboooks.tistory.com/187
+   **/
+  const btnStyle =
+    'bg-red-500 text-white w-full p-6 rounded-xl mb-3 h-32 whitespace-pre-line';
 
-  // interface TestDoc {
-  //   title: string;
-  //   firstQuestion: string;
-  //   lastQuestion: string;
-  // }
-  // { title, firstQuestion, lastQuestion }: TestDoc
   const TestDoc = () => {
     return (
       <div className='flex flex-col'>
