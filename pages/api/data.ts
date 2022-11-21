a185d7da74f38eb4cb126e52b41cf9ae4aac1d45
Ju eun <110323867+cobaltune@users.noreply.github.com>
2022-11-22 00:16:17 +0900

    feat: 질문지, 답안 줄바꿈 수정

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -9,17 +9,21 @@ export const mockupData: Data[] = [
   {
     question: `1. 당구공 마을에 초대받았다!
     초대장을 늦게 받아 출발 시간이 얼마 남지 않았다. 이때 나는?`,
-    firstAnswer: { desc: `'당구공 마을? 너무 기대된다 설레>< 뭐 입고가지?'`, type: 'P' },
+    firstAnswer: { desc: `'당구공 마을? 너무 기대된다 설레><
+    뭐 입고가지?'`, type: 'P' },
     secondAnswer: { desc: `'출발 장소까지 가는 데 30분 걸리니까 정각까지 준비하면 10분 여유롭게 도착하겠다.'`, type: 'J' },
   },
   {
     question: `2. 마을 입구에 도착하니 주민들이 앞구르기를 하며 입장하고 있다. 이때 나는?`,
-    firstAnswer: { desc: `'이 마을은 원래 이렇게 입장하나 보다.' 하며 앞구르기를 한다`, type: 'S' },
-    secondAnswer: { desc: `'왜 앞구르기를 하면서 들어가는 거지?' 하는 의문을 가진다.`, type: 'N' },
+    firstAnswer: { desc: `'이 마을은 원래 이렇게 입장하나 보다.'
+    하며 앞구르기를 한다`, type: 'S' },
+    secondAnswer: { desc: `'왜 앞구르기를 하면서 들어가는 거지?'
+    하는 의문을 가진다.`, type: 'N' },
   },
   {
     question: `3. 숙소 키를 받아 방에 들어가 보니, 내 방에만 웰컴 쿠키가 없는 거 같다. 이때 나는?`,
-    firstAnswer: { desc: `'왜 나만 없지..?' 누군가 알아차려 줄 때까지 기다린다.`, type: 'I' },
+    firstAnswer: { desc: `'왜 나만 없지..?'
+    누군가 알아차려 줄 때까지 기다린다.`, type: 'I' },
     secondAnswer: { desc: `곧바로 프론트에 전화해서 물어본다.`, type: 'E' },
   },
   {
@@ -29,12 +33,15 @@ export const mockupData: Data[] = [
   },
   {
     question: `5. 마을 축제가 열린다는 카탈로그를 받았다. 이때 나는?`,
-    firstAnswer: { desc: `몇 시에 무슨 행사가 잡혀 있는지 처음부터 차분히 읽어본다.`, type: 'S' },
-    secondAnswer: { desc: `전체적으로 한번 훑어보고 관심 있는 거 몇 개만 기억해둔다.`, type: 'N' },
+    firstAnswer: { desc: `몇 시에 무슨 행사가 잡혀 있는지
+    처음부터 차분히 읽어본다.`, type: 'S' },
+    secondAnswer: { desc: `전체적으로 한번 훑어보고
+    관심 있는 거 몇 개만 기억해둔다.`, type: 'N' },
   },
   {
     question: `6. 마을 축제에 다양한 행사가 준비되어 있다. 참여하고 싶은 곳은?`,
-    firstAnswer: { desc: `혼자 여유롭게 들을 수 있는 동물 합창단의 음악 감상회`, type: 'I' },
+    firstAnswer: { desc: `혼자 여유롭게 들을 수 있는
+    동물 합창단의 음악 감상회`, type: 'I' },
     secondAnswer: { desc: `다 함께 즐기는 개구리의 댄스파티`, type: 'E' },
   },
   {
@@ -45,7 +52,8 @@ export const mockupData: Data[] = [
   {
     question: `8. 곰과 오리와 함께 노래자랑에 나가기로 했다. 곰이 메인보컬을 하고 싶어 하는데 노래는 오리가 제일 잘한다. 나의 반응은?`,
     firstAnswer: { desc: `그래! 대신 열심히 연습해 보자!`, type: 'F' },
-    secondAnswer: { desc: `그래도 메인보컬은 오리가 하는 게 좋을 거 같은데?`, type: 'T' },
+    secondAnswer: { desc: `그래도 메인보컬은 오리가 하는 게
+    좋을 거 같은데?`, type: 'T' },
   },
   {
     question: `9. 노래를 부르다 내 파트에서 삑사리가 났다. 이때 나는?`,
@@ -54,13 +62,16 @@ export const mockupData: Data[] = [
   },
   {
     question: `10. 숙소에 들어왔는데 방이 엉망이다.`,
-    firstAnswer: { desc: `피곤해 죽을 거 같은데.. 자고 내일 치워야겠다~`, type: 'P' },
+    firstAnswer: { desc: `피곤해 죽을 거 같은데..
+    자고 내일 치워야겠다~`, type: 'P' },
     secondAnswer: { desc: `피곤하지만 치우고 편하게 자야지~`, type: 'J' },
   },
   {
     question: `11. 집으로 돌아가기 전날, 동물 친구들에게 줄 머핀을 굽기로 했다.`,
-    firstAnswer: { desc: `한 이만큼 구우면 인당 하나씩 줄 수 있겠지?`, type: 'N' },
-    secondAnswer: { desc: `인당 하나씩 주려면.. 10개만 구우면 되겠다!`, type: 'S' },
+    firstAnswer: { desc: `한 이만큼 구우면
+    인당 하나씩 줄 수 있겠지?`, type: 'N' },
+    secondAnswer: { desc: `인당 하나씩 주려면..
+    10개만 구우면 되겠다!`, type: 'S' },
   },
   {
     question: `12. 당구공 마을에서의 마지막 날, 헤어지기 아쉬워서 우는 강아지 친구에게 나는`,
