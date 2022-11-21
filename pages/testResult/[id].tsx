a9fe06e7bcdb665c4550e98ae6b67f284c12eff9
park-siwan <siwandevelop@gmail.com>
2022-11-21 23:53:44 +0900

    feat: 결과에 맞는 캐릭터명 표현

--- pages/testResult/[id].tsx
+++ pages/testResult/[id].tsx
@@ -1,10 +1,35 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 
+interface MbtiResult {
+  [x: string]: string;
+}
+
+const result: MbtiResult = {
+  INTJ: '0번젖소',
+  ISTJ: '0번젖소',
+  ENFJ: '1번쿼카',
+  ISFP: '2번코끼리',
+  ISTP: '2번코끼리',
+  ENTP: '3번토끼',
+  ESTP: '3번토끼',
+  ENFP: '4번개구리',
+  ISFJ: '5번오리',
+  INFJ: '5번오리',
+  ESFJ: '6번돼지',
+  ESFP: '7번강아지',
+  INFP: '8번곰',
+  INTP: '8번곰',
+  ESTJ: '9번기린',
+  ENTJ: '9번기린',
+};
+
 export default function TestResult() {
   const route = useRouter();
-  console.log('route', route);
-  return <div>당신은 {route.query.id}</div>;
+  const { id } = route.query;
+  const idResult = id as string;
+
+  return <div>당신은 {result[idResult]}</div>;
 }
 // export async function getStaticPaths() {
 //   // const postData = getPostData(params.id);
