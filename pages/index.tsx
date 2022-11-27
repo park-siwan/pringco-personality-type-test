fd366a87561e2c046771f56800b016b8a29e9dab
park-siwan <siwandevelop@gmail.com>
2022-11-28 00:22:14 +0900

    feat: 메인 글귀 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,9 @@ import { useState } from 'react';
 import mainBilliards from '../public/images/main-billiards.png';
 import hourglass from '../public/images/hourglass.png';
 import mainFooter from '../public/images/main-footer.png';
-import startBtn from '../public/images/start-btn.svg';
-import { css } from '@emotion/react';
+import startBtn from '../public/images/start-btn.png';
+import mainTalk from '../public/images/main-talk.png';
+
 import Footer from '../src/organism/Footer';
 export default function Home() {
   const route = useRouter();
@@ -16,7 +17,9 @@ export default function Home() {
     route.push('/test');
   };
   return (
-    <div className='max-h-screen overflow-hidden'>
+    <div
+    // className='max-h-screen overflow-hidden'
+    >
       <Head>
         {/* What is the ball you chose? */}
         <title>8BALL FRIENDS</title>
@@ -32,9 +35,14 @@ export default function Home() {
             className='mb-[23px]'
           />
         </div>
+        <div className='flex justify-center items-center relative'>
+          <Image src={mainTalk} alt='메인 글' className='mb-[23px]' />
+          <h3 className='absolute mx-8 font-bold mb-3 text-center text-[13px]'>
+            당구공 마을에 사는 데굴데굴 당구공동물들 나의 당구공은 누구일까?
+          </h3>
+        </div>
         <button
           onClick={handleTestStart}
-          // className='bg-[#D7D4CF] text-[#110306] w-full p-3 mb-[49px]'
           className='flex justify-center w-full mb-[10%]'
         >
           <Image src={startBtn} alt='What is the ball you chose?' />
