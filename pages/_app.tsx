61c49447352983da9977ec4e9773851ac26f630f
park-siwan <siwandevelop@gmail.com>
2022-11-28 03:42:49 +0900

    fix: kakao typescript error fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { css } from '@emotion/react';
-
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div
