19b24f3d2b050bcc744dceaef6bf1b7ef63597a7
park-siwan <siwandevelop@gmail.com>
2022-11-28 00:56:21 +0900

    fix: 메인스타일오류

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ export default function Home() {
           <div className='relative w-[296px] flex justify-center items-center'>
             <Image src={mainTalk} alt='메인 글' className='' />
             <h3 className='absolute font-bold mb-3 text-center text-[13px] m-4'>
-              당구공 마을에 사는 데굴데굴 당구공동물들 나의 당구공은 누구일까?
+              당구공 마을에 사는 데굴데굴 당구공 동물들 나의 당구공은 누구일까?
             </h3>
           </div>
         </div>
