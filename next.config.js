bc7c0d2e97ff903e09b94fd0d28e194103c4982b
park-siwan <siwandevelop@gmail.com>
2022-11-30 02:39:34 +0900

    feat: 이전에 공유된 주소를 위해 현재 구조에 맞게 리다이렉팅

--- next.config.js
+++ next.config.js
@@ -1,10 +1,97 @@
 /** @type {import('next').NextConfig} */
+const baseURL = '/testResult/';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     emotion: true,
   },
+  async redirects() {
+    return [
+      {
+        source: `${baseURL}INTJ`,
+        destination: `${baseURL}milk-cow1`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ISTJ`,
+        destination: `${baseURL}milk-cow2`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ENFJ`,
+        destination: `${baseURL}quokka`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ISFP`,
+        destination: `${baseURL}elephant1`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ISTP`,
+        destination: `${baseURL}elephant2`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ENTP`,
+        destination: `${baseURL}rabbit1`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ESTP`,
+        destination: `${baseURL}rabbit2`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ENFP`,
+        destination: `${baseURL}frog`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ISFJ`,
+        destination: `${baseURL}duck1`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}INFJ`,
+        destination: `${baseURL}duck2`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ESFJ`,
+        destination: `${baseURL}pig`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ESFP`,
+        destination: `${baseURL}puppy`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}INFP`,
+        destination: `${baseURL}bear1`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}INTP`,
+        destination: `${baseURL}bear2`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ESTJ`,
+        destination: `${baseURL}giraffe1`,
+        permanent: false,
+      },
+      {
+        source: `${baseURL}ENTJ`,
+        destination: `${baseURL}giraffe2`,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
+``;
